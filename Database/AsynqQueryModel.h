128cc19984f51c50764384d8e2c0032592f06f2a
Andreas Misje <amisje@gmail.com>
2016-07-11 07:53:55 +0100

    Add missing default arguments in overriden AsyncQueryModel methods

--- Database/AsynqQueryModel.h
+++ Database/AsynqQueryModel.h
@@ -36,10 +36,11 @@ public:
 
 	/** @name QAbstractItemModel interface */
 	///@{
-	int rowCount(const QModelIndex &parent) const;
-	int columnCount(const QModelIndex &parent) const;
-	QVariant data(const QModelIndex &index, int role) const;
-	QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+	int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+	int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+	QVariant headerData(int section, Qt::Orientation orientation, int role = 
+			Qt::DisplayRole) const override;
 	///@}
 
 protected slots:
